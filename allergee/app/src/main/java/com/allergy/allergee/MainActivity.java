9aebdad7aa60d56a7d781290bbe194047193fddb
Kevin Luong <kluon030@uottawa.ca>
2023-02-18 18:47:38 -0500

    added login page

--- allergee/app/src/main/java/com/allergy/allergee/MainActivity.java
+++ allergee/app/src/main/java/com/allergy/allergee/MainActivity.java
@@ -2,13 +2,35 @@ package com.allergy.allergee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn = (Button) findViewById(R.id.signIn);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn(view);
+            }
+        });
+    }
+
+    public void signIn(View view) {
+        Intent i = new Intent(MainActivity.this, Welcome.class);
+        startActivity(i);
     }
 }
